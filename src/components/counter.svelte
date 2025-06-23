5d8d04879a8393208880102ad4fd2509180e0f8e
Keshav <95571677+Keshav-writes-code@users.noreply.github.com>
2025-06-24 00:46:36 +0530

    refac: remove unwanted compoenents

--- src/components/counter.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts" >
-  let count = $state(0)
-</script>
-
-<button class="btn btn-lg btn-primary " onclick={()=>{ count++ }} >
-  {count}
-  <div class="i-tabler:click size-5"></div>
-</button>
