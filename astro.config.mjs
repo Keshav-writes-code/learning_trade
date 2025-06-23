f78687f64c7c35683d2ed9b17dce5839cc6e908e
Keshav <95571677+Keshav-writes-code@users.noreply.github.com>
2025-06-24 00:46:27 +0530

    feat: add astro db intergration module

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,12 @@ import { defineConfig } from "astro/config";
 import UnoCSS from "unocss/astro";
 import svelte from "@astrojs/svelte";
 
+import db from "@astrojs/db";
+
 export default defineConfig({
-  integrations: [
-    UnoCSS({
-      injectReset: true,
-    }),
-    svelte(),
-  ],
+  integrations: [UnoCSS({
+    injectReset: true,
+  }), svelte(), db()],
   site: "https://Keshav-writes-code.github.io",
   base: "DUAS-template",
 });
