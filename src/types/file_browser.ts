83693e09b5db0b7ed3863bf1794def93a6efd75a
Keshav <95571677+Keshav-writes-code@users.noreply.github.com>
2025-06-24 16:52:56 +0530

    feat: add pages for all the diffrent heirarchies

---
+++ src/types/file_browser.ts
@@ -0,0 +1,15 @@
+export type ExplorerItem =
+  | {
+      type: "folder";
+      label: string | number;
+      link: string | number;
+    }
+  | {
+      type: "file";
+      label: string | number;
+      link: string | number;
+      file_type: File_type;
+    };
+
+export type Item_type = "folder" | "file";
+export type File_type = "pdf" | "image";
