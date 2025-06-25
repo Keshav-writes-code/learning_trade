591e65bf68894206b684965838aab605f7cbebf9
Keshav <95571677+Keshav-writes-code@users.noreply.github.com>
2025-06-25 21:17:57 +0530

    refac: move google drive related function to googledrive module

--- src/gdrive/gdrive_functions.ts
+++ src/gdrive/gdrive_functions.ts
@@ -1,3 +1,9 @@
+
+export function make_gApis_from_gDrvie_link(gdrive_link: string, api: string) {
+  const match = gdrive_link.match(/\/file\/d\/([a-zA-Z0-9_-]{25,})/);
+  const fileId = match ? match[1] : null;
+  return `https://www.googleapis.com/drive/v3/files/${fileId}?alt=media&key=${api}`;
+}
 export function getDirectDownloadLink(sharedLink: string): string | null {
   const match = sharedLink.match(/\/file\/d\/([a-zA-Z0-9_-]{25,})/);
   const fileId = match?.[1];
